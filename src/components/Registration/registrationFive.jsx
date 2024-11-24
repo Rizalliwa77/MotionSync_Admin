0e24df1488e18a3c6c5f9f0b672e3f1c9295f468
Rizal <Rizalliwa76@gmail.com>
2024-11-25 00:37:15 +0800

    Resolve path

--- src/components/Registration/registrationFive.jsx
+++ src/components/Registration/registrationFive.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../../assets/Registration/registrationFive.css";
+import "../../assets/Registration/RegistrationFive.css";
 
 const RegistrationFive = () => {
   const navigate = useNavigate();
