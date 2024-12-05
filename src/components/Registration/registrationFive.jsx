d0e1f7d0ca1bb56b4a64cea1155a54e2c1b47d57
Rizal <Rizalliwa76@gmail.com>
2024-12-06 00:56:59 +0800

    debugging work 10

--- src/components/Registration/registrationFive.jsx
+++ src/components/Registration/registrationFive.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './registrationFive.css';
+import '../../assets/Registration/registrationFive.css';
 
 const RegistrationFive = () => {
   const navigate = useNavigate();
